9a0e2b01f9434fade7ef737ed02271fb57d4d0eb
Ismerai <54740257+ismeraidavid@users.noreply.github.com>
2024-08-21 17:16:45 +0100

    Añadida la funcionalidad de los botones
    Se ha añadido la funcionalidad de los botones para cambiar el color y
    limpiar el tablero.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+// Color para pintar
+
+let color = "black";
+
+// Funciones del tablero
+
+function changeSizeBoard(sizeBoard) {
+    if (sizeBoard < 0 || sizeBoard > 100) {
+        alert("Escoge un valor entre 2 y 100")
+        
+    } else {
+        populateBoard(sizeBoard);
+    }
+    
+};
+
 function populateBoard (sizeBoard) {
     const board = document.querySelector(".board");
     let squares = board.querySelectorAll("div");
@@ -8,20 +24,35 @@ function populateBoard (sizeBoard) {
     totalSquares = sizeBoard * sizeBoard;
     for (i = 0 ; i < totalSquares; i++) {
         const square = document.createElement("div");
-        square.style.backgroundColor = "red";
+        square.addEventListener("mouseover", colorSquare);
+        square.style.backgroundColor = "white";
         board.insertAdjacentElement("beforeend", square);
     }
 }
 
-function changeSizeBoard(sizeBoard) {
-    if (sizeBoard < 0 || sizeBoard > 100) {
-        alert("Escoge un valor entre 2 y 100")
-        
-    } else {
-        populateBoard(sizeBoard);
-    }
-    
+function resetBoard () {
+    const board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach(div => div.style.backgroundColor = "white");
 }
+
+
+
+// Funciones para el color de los cuadrados
+
+function colorSquare() {
+    if (color ==="random") {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+};
+
+function changeColor (colorChoice) {
+    color = colorChoice;
+};
+
+
 
 const changeSizeBtn = document.querySelector("#changeSize");
 const inputSizeBoard = document.querySelector("#inputSize");
